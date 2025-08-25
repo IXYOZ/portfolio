0407a6eb001f7e4798157fffd65bbf6bd3f19809
IXYOZ <nuttapon9.kn@gmail.com>
2025-08-25 14:57:44 +0930

    Change words&icon

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,34 +1,38 @@
 'use client'
 
+import { FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa"
+import { HiOutlineMail } from "react-icons/hi"
 export default function Contact(){
 
     return <section id="contact" className="mx-auto max-w-6xl px-4 py-16">
     <div className="rounded-3xl border border-white/10 bg-white/5 p-8 text-center shadow">
       <h2 className="text-2xl font-semibold md:text-3xl">
-        {"Yes I can do it anyway, let's gows!."}
+        {"Yes I can, let's gows!"}
       </h2>
       <p className="mx-auto mt-3 max-w-xl text-sm text-white/70 md:text-base">
         Open to full-time , contract roles and internships in Australia.
-        Email me or say hello on my LinkedIn.
+        Email me or say hello on my LinkedIn also Whatsapp.
       </p>
-      <div className="mt-5 flex flex-wrap justify-center gap-3">
+      <div className="mt-5 flex flex-wrap justify-center gap-4 text-white text-2xl">
         <a
-          href="mailto:nuttapon9.kn@gmail.com"
-          className="rounded-2xl bg-white px-4 py-2 text-sm font-medium text-slate-900 hover:shadow-lg"
+          href="mailto:nuttapon9.kn@gmail.com" target="_blank" rel="noopener noreferrer"
         >
-          Email
+          <HiOutlineMail/>
         </a>
         <a
-          href="https://www.linkedin.com/in/nuttapon9-khampoopen"
-          className="rounded-2xl bg-white px-4 py-2 text-sm font-medium text-slate-900 hover:shadow-lg"
+          href="https://www.linkedin.com/in/nuttapon9-khampoopen" target="_blank" rel="noopener noreferrer"
         >
-          LinkedIn
+          <FaLinkedin/>
         </a>
         <a
-          href="https://github.com/IXYOZ"
-          className="rounded-2xl bg-white px-4 py-2 text-sm font-medium text-slate-900 hover:shadow-lg"
+          href="https://github.com/IXYOZ" target="_blank" rel="noopener noreferrer"
         >
-          GitHub
+          <FaGithub/>
+        </a>
+        <a
+          href="https://wa.me/0499763044" target="_blank" rel="noopener noreferrer"
+        >
+          <FaWhatsapp/>
         </a>
       </div>
     </div>
